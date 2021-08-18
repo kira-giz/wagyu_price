c7fc0bd3f175ad3d8a067cc7e8a0514616e4223c
kira-giz <pyscripthon0426@gmail.com>
2021-08-18 12:31:06 +0900

    update: スクロールボタンのレスポンシブ対応

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 $(function () {
-  $pageTop = $("#page-top");
+  var timer = false;
+  $(window).resize(function () {
+    if (timer !== false) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      location.reload();
+    }, 200);
+  });
+
+  var $pageTop = $("#page-top");
   $pageTop.hide();
-  $(window).scroll(function () {
+  $(window).on('load resize', function () {
+    if (window.matchMedia("(max-width: 575px)").matches) {
+      $(window).on("scroll", function () {
+        btnFade();
+      });
+      $pageTop.on("click", function () {
+        returnTop();
+      });
+    } else {
+      $pageTop.stop().hide();
+    };
+  });
+
+  function btnFade() {
     // 40はヘッダーの高さ
     if ($(window).scrollTop() > 40) {
-      $pageTop.fadeIn(300);
+      $pageTop.stop().fadeIn();
     } else {
-      $pageTop.fadeOut();
+      $pageTop.stop().fadeOut();
     }
-  });
-  $pageTop.on("click", function () {
-    $("body,html").animate({ scrollTop: 0 }, 300);
+  };
+
+  function returnTop() {
+    $("body,html").animate({ scrollTop: 0 }, 200);
+    console.log($(window).width());
     // aタグのイベント無効
     return false;
-  });
+  };
 });
