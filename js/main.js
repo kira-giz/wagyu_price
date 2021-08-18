09331963a3655c4371b21cc3ce1788832a894ee8
kira-giz <pyscripthon0426@gmail.com>
2021-08-18 14:55:41 +0900

    update: スクロールボタンのクリックとスクロールイベントを再定義

--- js/main.js
+++ js/main.js
@@ -1,42 +1,29 @@
 $(function () {
-  var timer = false;
-  $(window).resize(function () {
-    if (timer !== false) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(function () {
-      location.reload();
-    }, 200);
+  var $pageTop = $("#page-top");
+
+  $pageTop.hide();
+  $pageTop.on("click", function () {
+    returnTop();
+  });
+  $(window).on("scroll", function () {
+    btnFade();
   });
 
-  var $pageTop = $("#page-top");
-  $pageTop.hide();
-  $(window).on('load resize', function () {
-    if (window.matchMedia("(max-width: 575px)").matches) {
-      $(window).on("scroll", function () {
-        btnFade();
-      });
-      $pageTop.on("click", function () {
-        returnTop();
-      });
-    } else {
-      $pageTop.stop().hide();
-    };
-  });
+  function returnTop() {
+    $("body,html").animate({ scrollTop: 0 }, 200);
+    return false;
+  };
 
   function btnFade() {
     // 40はヘッダーの高さ
     if ($(window).scrollTop() > 40) {
-      $pageTop.stop().fadeIn();
+      if (window.matchMedia("(max-width: 575px)").matches) {
+        $pageTop.stop().fadeIn();
+      };
     } else {
-      $pageTop.stop().fadeOut();
+      if (window.matchMedia("(max-width: 575px)").matches) {
+        $pageTop.stop().fadeOut();
+      };
     }
-  };
-
-  function returnTop() {
-    $("body,html").animate({ scrollTop: 0 }, 200);
-    console.log($(window).width());
-    // aタグのイベント無効
-    return false;
   };
 });
